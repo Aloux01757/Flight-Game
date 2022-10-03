5ef83f685a7d8a9a2ad359f9ab33df200e364f17
Aloux01757 <arthurspring2@gmail.com>
2022-10-03 14:57:03 -0300

    Behavior movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,6 +13,28 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Flying");
+        }
+
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+         if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
     }
 }
