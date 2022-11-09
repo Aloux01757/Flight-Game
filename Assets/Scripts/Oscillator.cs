9ff9138b12cb604d2fdd6c41b2564c79edd48a8f
Arthur <arthurspring2@gmail.com>
2022-11-08 21:27:52 -0300

    Made behavior script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
 
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movemnetFactor;
+    [SerializeField] [Range(0,1)] float movemnetFactor; // Vai determinar o ponto de refereincia até o destino
+    [SerializeField] float period = 2f;
     Vector3 startingPosition; 
 
     void Start()
@@ -16,6 +17,14 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        float cycles = Time.time / period; // deixar crescendo no looping 
+        const float tau = Mathf.PI * 2; // costante de valor 6.283 (Ciruculo completo)
+
+        float rawSinWave = Mathf.Sin(cycles * tau); // -1 para 1
+
+        movemnetFactor = (rawSinWave + 1f)  /2f; // recalcular 0 para 1
+
+
         Vector3 Offeset = movementVector * movemnetFactor;
         transform.position = startingPosition + Offeset; // return the last position
     }
